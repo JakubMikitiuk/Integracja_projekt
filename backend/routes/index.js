2549e810ad65c92cd18867a18843036efec596bf
Jakub_PC <s98057@pollub.edu.pl>
2024-06-01 19:29:18 +0200

    Wykres,dane,Pobieranie,sciezki

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -35,36 +35,48 @@ const routes = (app) => {
       });
   });
 
-  router.post("/api/import-csv", (req, res) => {
-    fs.createReadStream(path.join(__dirname, '..', 'data', 'Zgony.csv'))
-      .pipe(csv())
-      .on('data', (row) => {
-        const deathCount = new DeathCount({
-          week: row['Nr tygodnia'],
-          year2016: row['2016'],
-          year2017: row['2017'],
-          year2018: row['2018'],
-          year2019: row['2019'],
-          year2020: row['2020'],
-          year2021: row['2021'],
-          year2022: row['2022'],
-          year2023: row['2023'],
-        });
+  router.post("/import-csv", (req, res) => {
+    DeathCount.countDocuments({}, (err, count) => {
+      if (err) {
+        console.error(e);
+        serverResponses.sendError(res, messages.BAD_REQUEST, err);
+      } else if (count > 0) {
+        
+        serverResponses.sendError(res, messages.NOT_FOUND, 'Collection is not empty');
+        console.error('Collection is not empty');
+      } else {
+        const deathCounts = [];
   
-        deathCount
-          .save()
-          .then((result) => {
-            serverResponses.sendSuccess(res, messages.SUCCESSFUL, result);
+        fs.createReadStream(path.join(__dirname, '..', 'data', 'Zgony.csv'))
+          .pipe(csv())
+          .on('data', (row) => {
+            const deathCount = new DeathCount({
+              week: row['Nr tygodnia'],
+              year2016: row['2016'],
+              year2017: row['2017'],
+              year2018: row['2018'],
+              year2019: row['2019'],
+              year2020: row['2020'],
+              year2021: row['2021'],
+              year2022: row['2022'],
+              year2023: row['2023'],
+            });
+  
+            deathCounts.push(deathCount);
           })
-          .catch((e) => {
-            serverResponses.sendError(res, messages.BAD_REQUEST, e);
+          .on('end', () => {
+            Promise.all(deathCounts.map(deathCount => deathCount.save()))
+              .then((results) => {
+                serverResponses.sendSuccess(res, messages.SUCCESSFUL, results);
+              })
+              .catch((e) => {
+                console.error(e);
+                serverResponses.sendError(res, messages.BAD_REQUEST, e);
+              });
           });
-      })
-      .on('end', () => {
-        console.log('CSV file successfully processed');
-      });
+      }
+    });
   });
-
   //it's a prefix before api it is useful when you have many modules and you want to
   //differentiate b/w each module you can use this technique
   app.use("/api", router);
